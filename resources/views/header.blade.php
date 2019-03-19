b0bad8843d8d3145622ddd160a2f76b7768d20de
chengwei.shiu <cheng-wei.shiu@ablerex.com.tw>
2019-03-19 10:59:18 +0800

    '完成購物車導覽列前端'

---
+++ resources/views/header.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand mr-auto">神奇購物網站</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ml-auto">
+          <li class='mr-3'><a class="nav-item nav-link" href="#"><i class="fas fa-home"></i> 回首頁<span class="sr-only">(current)</span></a></li>
+          <li class='mr-3'><a class="nav-item nav-link" href="#"><i class="fas fa-shopping-cart"></i>
+            購物車<span class="sr-only">(current)</span></a></li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                登入使用者
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">登入</a>
+                <a class="dropdown-item" href="#">註冊</a>
+              </div>
+          </li>
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.master')
+@section('title')
+My shop car
+@endsection
+@section('content')
+
+@endsection
+
